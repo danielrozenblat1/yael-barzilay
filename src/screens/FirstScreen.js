2b1eb3e23a5c14757ca75eefed1f8a75721762df
Your Name <you@example.com>
2024-06-16 16:18:22 +0300

    newLoader

--- src/screens/FirstScreen.js
+++ src/screens/FirstScreen.js
@@ -1,57 +1,74 @@
-import { useEffect } from "react"
-import NavBar from "../components/NavBar/NavBar"
-import Button from "../components/button/Button"
-import styles from "./FirstScreen.module.css"
-import ScrollReveal from "scrollreveal"
+import { useState, useEffect } from "react";
+import NavBar from "../components/NavBar/NavBar";
+import Button from "../components/button/Button";
+import styles from "./FirstScreen.module.css";
+import ScrollReveal from "scrollreveal";
+import Loader from "../components/Loader/Loader";
+import background from "../images/רקע יעל ברזילאי.png"
 
-const FirstScreen=()=>{
-   useEffect(()=>{
-    ScrollReveal().reveal(`.${styles.name}`, {
-        duration: 1000,
-        distance: "30px",
-        origin: "top", // Start from the right side
-        easing: "ease-out",
-        reset:false,
-        viewFactor: 0.2,
-        interval: 300, // Delay between each element
-        delay: 200, // Delay before the animation starts
-        scale: 1, // Set scale to 1 or null
-      });
-      ScrollReveal().reveal(`.${styles.title}`, {
-        duration: 1000,
-        distance: "30px",
-        origin: "bottom", // Start from the right side
-        easing: "ease-out",
-        reset:false,
-        viewFactor: 0.2,
-        interval: 300, // Delay between each element
-        delay: 200, // Delay before the animation starts
-        scale: 1, // Set scale to 1 or null
-      });
-      ScrollReveal().reveal(`.${styles.btn}`, {
-        duration: 1000,
-        distance: "30px",
-        origin: "bottom", // Start from the right side
-        easing: "ease-out",
-        reset:false,
-        viewFactor: 0.2,
-        interval: 300, // Delay between each element
-        delay: 200, // Delay before the animation starts
-        scale: 1, // Set scale to 1 or null
-      });
-   },[])
-return <>
+const FirstScreen = () => {
+    const [isImageLoaded, setIsImageLoaded] = useState(false);
 
-<div className={styles.background}>
+    useEffect(() => {
+        const img = new Image();
+        img.src = background ; // Replace with the actual path to your background image
+        img.onload = () => {
+            setIsImageLoaded(true);
+        };
+    }, []);
 
-</div>
+    useEffect(() => {
+        if (isImageLoaded) {
+            ScrollReveal().reveal(`.${styles.name}`, {
+                duration: 1000,
+                distance: "30px",
+                origin: "top",
+                easing: "ease-out",
+                reset: false,
+                viewFactor: 0.2,
+                interval: 300,
+                delay: 200,
+                scale: 1,
+            });
+            ScrollReveal().reveal(`.${styles.title}`, {
+                duration: 1000,
+                distance: "30px",
+                origin: "bottom",
+                easing: "ease-out",
+                reset: false,
+                viewFactor: 0.2,
+                interval: 300,
+                delay: 200,
+                scale: 1,
+            });
+            ScrollReveal().reveal(`.${styles.btn}`, {
+                duration: 1000,
+                distance: "30px",
+                origin: "bottom",
+                easing: "ease-out",
+                reset: false,
+                viewFactor: 0.2,
+                interval: 300,
+                delay: 200,
+                scale: 1,
+            });
+        }
+    }, [isImageLoaded]);
 
-<h1 className={styles.name}>יעל ברזילאי</h1>
-<h2 className={styles.title}>להפוך למותג ברגע</h2>
-<div className={styles.btn}><Button text="לפרוייקטים" to="פרוייקטים"/></div>
+    if (!isImageLoaded) {
+        return <Loader />;
+    }
 
-</>
+    return (
+        <>
+            <div className={styles.background}></div>
+            <h1 className={styles.name}>יעל ברזילאי</h1>
+            <h2 className={styles.title}>להפוך למותג ברגע</h2>
+            <div className={styles.btn}>
+                <Button text="לפרוייקטים" to="פרוייקטים" />
+            </div>
+        </>
+    );
+};
 
-
-}
-export default FirstScreen
+export default FirstScreen;
